07acd1c3e0326444383e2123f7178cac096c87b2
zhoujin <913751531@qq.com>
2019-05-24 18:02:54 +0800

    vue-router

--- webpack.config.js
+++ webpack.config.js
@@ -1,123 +1,115 @@
 const path = require('path')
-const { VueLoaderPlugin } = require('vue-loader')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
-const config = {
-    entry: path.join(__dirname, 'src/index.js'),
-    output: {
-        filename: "budle.[hash:8].js",
-        path: path.join(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.jsx/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            // {
-            //     test: /\.styl/,
-            //     use: [
-            //         'style-loader',
-            //         'css-loader',
-            //         {
-            //             loader: 'postcss-loader',
-            //             options: {
-            //                 sourceMap: true,
-            //             }
-            //         },
-            //         'stylus-loader'
-            //     ]
-            // },
-            {
-                test: /\.(gif|jpg|jpeg|png|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024,
-                            name: '[name]-aaa.[ext]'
 
-                        }
-                    }
-                ]
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, 'src/client/index.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, './src/public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-aaa.[ext]'
             }
+          }
         ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: isDev ? '"development"' : '"production"'
-            }
-        }),
-        new HTMLPlugin()
+      }
     ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
+  ]
 }
 
 if (isDev) {
-    config.module.rules.push({
-        test: /\.styl/,
-        use: [
-            'style-loader',
-            'css-loader',
-            {
-                loader: 'postcss-loader',
-                options: {
-                    sourceMap: true,
-                }
-            },
-            'stylus-loader'
-        ]
-    })
-    config.devtool = '#cheap-module-eval-source-map'
-    config.devServer = {
-        port: 8000,
-        host: '0.0.0.0',
-        overlay: {
-            errors: true,
-        },
-        hot: true,
-    }
-    // 热加载
-    config.plugins.push(
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
-    )
+  config.module.rules.push({
+    test: /\.styl/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+        }
+      },
+      'stylus-loader'
+    ]
+  })
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: 8003,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,
+    },
+    historyApiFallback: {
+        index: '/public/index.html'
+    },
+    hot: true
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
 } else {
-    config.output.filename = '[name].[chunkHash:8].js'
-    config.module.rules.push({
-        test: /\.styl/,
-        use: ExtractPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-                'css-loader',
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                        sourceMap: true,
-                    }
-                },
-                'stylus-loader'
-            ]
-        })
+  config.entry = {
+    app: path.join(__dirname, 'src/client/index.js'),
+    vendor: ['vue']
+  }
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push(
+    {
+      test: /\.styl/,
+      use: ExtractPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    },
+  )
+  config.plugins.push(
+    new ExtractPlugin('styles.[contentHash:8].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
     })
-    config.plugins.push(
-        new ExtractPlugin('styles.[hash:8].css')
-    )
+  )
 }
 
 module.exports = config
